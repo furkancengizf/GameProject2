54dd9692c65010b33b85597e6fc62df39116caec
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 00:59:06 +0300

    Update GamerManager.java

--- GamerManager.java
+++ GamerManager.java
@@ -1,1 +1,18 @@
 
+package GameProject;
+
+public class GamerManager implements GameService{
+  @Override
+  public void Add(){
+    System.out.printlyn("Oyuncu Sisteme Eklendi")
+  }
+  @Override
+  public void Delete(){
+    System.out.printlyn("Oyuncu Sistemden Silindi")
+  }
+  @Override
+  public void Update(){
+    System.out.printlyn("Oyuncu Sistemde Güncellendi")
+  }
+  
+}
