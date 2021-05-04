8e3507a8b06fc97ed747f9cf29cfbfaab99ba0fd
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 00:51:46 +0300

    Update CampaignManager.java

--- CampaignManager.java
+++ CampaignManager.java
@@ -1,1 +1,24 @@
 
+package GameProject;
+
+public class CampaignManager implements CampaignService{
+
+	@Override
+	public void add(Campaign campaign){
+	  System.out.println(campaign.getName() + " isimli kampanya sisteme kayýt edildi.");
+		
+	}
+
+	@Override
+	public void update(Campaign campaign) {
+		System.out.println(campaign.getName() + " isimli kampanya sistemde güncellendi.");
+		
+	}
+
+	@Override
+	public void delete(Campaign campaign) {
+System.out.println(campaign.getName() + " isimli kampanya kaydý silindi.");
+		
+	}
+	
+}
