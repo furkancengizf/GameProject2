3f89db3945e1327ee04b9524bf02e28f99e3d783
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 00:55:45 +0300

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -1,1 +1,18 @@
 
+package GameProject;
+
+
+public class GameManager implements GameService{
+  @Override
+  public void Add(){
+    System.out.printlyn("Oyun Sisteme Eklendi")
+  }
+  @Override
+  public void Delete(){
+    System.out.printlyn("Oyun Sistemden Silindi")
+  }
+  @Override
+  public void Update(){
+    System.out printlyn("Oyun G"
+  }
+}
