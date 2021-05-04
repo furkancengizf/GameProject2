709161031e07888ddda160f7a162be90ba50ff67
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 01:01:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
 
+package GameProject;
+
+public class Main{
+  
+  public static void main(String[] args) 
+	{
+
+  Gamer Gamer = new Gamer(1,"Furkan","Cengiz",2003,11111111111)
+  Campaign Campaign = new Campaign("Oyun İndirimi")
+  Sale sale = new Sale("Oyun Satışı Yapıldı")
+  
+  
+  GamerManager gamerManager = new GamerManager(){
+  GamerManager.Add();
+  }
+  
+  CampaignManager campaignManager = new CampaignManager(){
+    CampaignManager.Delete
+  }
+  
+  SaleManager saleManager = new SaleManager(){
+    SaleManager.Update
+  }
+  
