773cce7d0c10f255a1263c0e1fafd44e2b7f9d2d
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 00:50:26 +0300

    Update Campaign.java

--- Campaign.java
+++ Campaign.java
@@ -1,1 +1,34 @@
 
+package GameProject;
+
+public class Campaign{
+  private String CampaignName;
+  private String CampaignDetail;
+  private int CampaignDiscount;
+
+
+
+public class Campaign(String CampaignName, String CampaignDetail, int CampaignDiscount){
+  
+  this.CampaignName = CampaignName;
+  this.CampaignDetail = CampaignDetail;
+  this.CampaignDiscount = CampaignDiscount;
+}
+
+public String getCampaignName() {
+		return campaignName;
+	}
+	public void setCampaignName(String campaignName) {
+		this.campaignName = campaignName;
+	}
+	public String getCampaignDetail() {
+		return campaignDetail;
+	}
+	public void setCampaignDetail(String campaignDetail) {
+		this.campaignDetail = campaignDetail;
+	}
+	public int getCampaignDiscount() {
+		return campaignDiscount;
+	}
+
+}
