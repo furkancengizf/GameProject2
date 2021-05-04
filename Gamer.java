6f0de7f8c96e4c33a98c95ec7899b9247c56cc8b
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 00:56:49 +0300

    Update Gamer.java

--- Gamer.java
+++ Gamer.java
@@ -1,1 +1,50 @@
 
+package GameProject;
+ 
+public class Gamer{
+  
+  private int id;
+  private String FirstName;
+  private String LastName;
+  private int borth;
+  private String  nationalityid;
+  
+}
+
+public class User(int id, String name, String LastName, int borth, String nationalityid){
+  this id = id;
+  this FirstName = FirstName;
+}
+
+
+public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+	public String getNationalityId() {
+		return nationalityId;
+	}
+	public void setNationalityId(String nationalityId) {
+		this.nationalityId = nationalityId;
+	}
+	
