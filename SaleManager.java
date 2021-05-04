1eaa6a0b9452d8f60b4e30235582905681f1c63c
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 01:03:46 +0300

    Update SaleManager.java

--- SaleManager.java
+++ SaleManager.java
@@ -1,1 +1,14 @@
 
+package GameProject;
+
+public class SaleManager implements SaleService{
+  public void AddToCard(){
+    System.out.printlyn("Ödeme Kartı Eklendi")
+  }
+  public void DeleteToCard(){
+    System.out.printlyn("Ödeme kartı Silindi")
+  }
+  public void BuyToGame(){
+    System.out.printlyn("Oyun Satın Alındı")
+  }
+}
