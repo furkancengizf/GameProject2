d04ab396d62240970517ae8dab7a82e5013889a4
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 00:54:58 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,1 +1,37 @@
 
+package GameProject;
+
+public class Game{
+  private String GameName;
+  private String GameDetail;
+  private int GamePrice;
+  
+}
+
+public class Game(String GameName, String GameDetail, int GamePrice){
+  this.GameName = GameName;
+  this.GameDetail = GameDetail;
+  this.GamePrice = GamePrice;
+}
+
+public String get.GameName(){
+  return GameName
+}
+
+public void set.GameName(){
+  this.GameName = GameName
+}
+
+public String get.GameDetail(){
+  return GameDetail
+}
+public void set.GameDetail(){
+  this.GameDetail = GameDetail
+}
+
+public int get.GamePrice(){
+  return GamePrice
+}
+public void set.GamePrice(){
+  this.GameDetail = GameDetail
+}
