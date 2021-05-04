8fc4e7ec6956d936d99aae11ef478d3413b445a5
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-05-05 01:02:43 +0300

    Update Sale.java

--- Sale.java
+++ Sale.java
@@ -1,1 +1,19 @@
 
+Package GameProject;
+
+public class Sale{
+  private int GamePrice;
+  
+}
+
+public class Sale(int GamePrice){
+  this.GamePrice = GamePrice;
+}
+
+public int get.GamePrice(){
+  return get.GamePrice
+}
+
+public int set.GamePrice(int GamePrice){
+  this.GamePrice = GamePrice
+}
